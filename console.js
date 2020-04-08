366c0465b19a21fd06a32ad91befe90255a6163c
bitmapper <bitmapper85@gmail.com>
2020-04-08 17:18:56 -0300

    added output io

--- console.js
+++ console.js
@@ -1,12 +1,32 @@
-var shen; (new Shen).then((x) => { shen = x })
+class OutStream {
+  constructor(console) {
+    this.console = console
+    this.write = (x) => {
+      this.console.Write(String.fromCharCode(x), 'jqconsole-output')
+    }
+  }
+}
+
+class InStream {
+  constructor() {
+    this.read = () => {}
+    this.close = () => {}
+  }
+}
+
 $(function () {
   var jqconsole = $('#console').jqconsole('', '~> ');
+  var shen; (new Shen({InStream, OutStream, stoutput: (new OutStream(jqconsole))})).then((x) => { shen = x });
   jqconsole.Write("Welcome to the (basic) ShenScript REPL!\n\n", 'jqconsole-output')
   var startPrompt = function () {
     jqconsole.Prompt(true, function (input) {
       shen.exec(input).then((y) => {  
         if (y != null) {
-          jqconsole.Write(y.toString() + '\n', 'jqconsole-output')
+          if (typeof(y) === "string") {
+            jqconsole.Write('"' + y + '"' + '\n', 'jqconsole-output')
+          } else {
+            jqconsole.Write(y.toString() + '\n', 'jqconsole-output')
+          }
         }
       })
      startPrompt();
