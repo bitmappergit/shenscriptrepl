c3cf7fac79e93130dd922e09e9d55f89139383d4
bitmapper <bitmapper85@gmail.com>
2020-04-09 18:36:17 -0300

    better repl

--- console.js
+++ console.js
@@ -14,23 +14,28 @@ class InStream {
   }
 }
 
-$(function () {
-  var jqconsole = $('#console').jqconsole('', '~> ');
-  var shen; (new Shen({InStream, OutStream, stoutput: (new OutStream(jqconsole))})).then((x) => { shen = x });
-  jqconsole.Write("Welcome to the (basic) ShenScript REPL!\n\n", 'jqconsole-output')
-  var startPrompt = function () {
-    jqconsole.Prompt(true, function (input) {
-      shen.exec(input).then((y) => {  
-        if (y != null) {
-          if (typeof(y) === "string") {
-            jqconsole.Write('"' + y + '"' + '\n', 'jqconsole-output')
-          } else {
-            jqconsole.Write(y.toString() + '\n', 'jqconsole-output')
-          }
-        }
-      })
-     startPrompt();
-   });
- };
- startPrompt();
+
+$(async function () {
+  var jqconsole = $('#console').jqconsole('', '');
+  var out = (new OutStream(jqconsole))
+  var shen = await new Shen({InStream, OutStream, stoutput: out, sterror: out})
+  //console.log(shen.valueOf("shen.*tc*"))
+  await shen.assign("*history*", 0)
+  await shen.exec("(define runsh Parsed -> (trap-error (shen.toplevel (read-from-string Parsed)) (/. E (shen.toplevel-display-exception E))))")
+  let evalShen = await shen.caller("runsh")
+
+  //jqconsole.Write("Welcome to the (basic) ShenScript REPL!\n\n", 'jqconsole-output')
+  await shen.exec('(do (shen.initialise_environment) (shen.credits))')
+  //jqconsole.Write("\n\n", 'jqconsole-output')
+
+  var startPrompt = async () => {
+    let prompt = await shen.exec('(make-string "~%~%(~A~A) " (value *history*) (if (= (value shen.*tc*) true) "+" "-"))')
+    jqconsole.SetPromptLabel(prompt)
+    jqconsole.Prompt(true, async (input) => {
+      evalShen(input)
+      await shen.exec("(set *history* (+ (value *history*) 1))")
+      startPrompt();
+    });
+  };
+  startPrompt();
 });
